b24ead0d59402cffa9f084736923173ad52811ab
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-20 00:22:02 +0100

    Add glass like screen

--- src/sections/Visualiser/classes/Components/ScreenFrame3D.ts
+++ src/sections/Visualiser/classes/Components/ScreenFrame3D.ts
@@ -2,16 +2,14 @@ import * as THREE from 'three';
 
 import { UpdateInfo } from 'utils/sharedTypes';
 
-import vertexShader from '../shaders/screenFrame/vertex.glsl';
-import fragmentShader from '../shaders/screenFrame/fragment.glsl';
 import { InteractiveObject3D } from './InteractiveObject3D';
 
 export class ScreenFrame3D extends InteractiveObject3D {
   static width = 5;
 
-  _mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.ShaderMaterial> | null = null;
+  _mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshPhysicalMaterial> | null = null;
   _geometry: THREE.PlaneGeometry | null = null;
-  _material: THREE.ShaderMaterial | null = null;
+  _material: THREE.MeshPhysicalMaterial | null = null;
 
   constructor() {
     super();
@@ -20,16 +18,21 @@ export class ScreenFrame3D extends InteractiveObject3D {
 
   _drawScreenFrame() {
     this._geometry = new THREE.PlaneBufferGeometry(ScreenFrame3D.width, ScreenFrame3D.width);
-    this._material = new THREE.ShaderMaterial({
+    this._material = new THREE.MeshPhysicalMaterial({
       side: THREE.DoubleSide,
-      vertexShader,
-      fragmentShader,
-      depthWrite: true,
-      depthTest: true,
-      uniforms: {
-        uTime: { value: 0 },
-        uRandom: { value: Math.random() },
-      },
+      color: 0xffffff,
+      metalness: 0,
+      roughness: 0,
+      transmission: 1,
+      ior: 1.5,
+      reflectivity: 0.7,
+      clearcoat: 1,
+      clearcoatRoughness: 0.1,
+      normalScale: new THREE.Vector2(0.3),
+      clearcoatNormalScale: new THREE.Vector2(0.2),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      thickness: 2.5,
     });
     this._mesh = new THREE.Mesh(this._geometry, this._material);
     this._mesh.position.y = ScreenFrame3D.width * 0.5;
