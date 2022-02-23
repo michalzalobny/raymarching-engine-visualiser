b72f7b473ad0cafac52933882ad869d16535f852
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-23 16:52:03 +0100

    Fix title

--- src/seo/Head/Head.tsx
+++ src/seo/Head/Head.tsx
@@ -12,7 +12,7 @@ interface HeadProps {
 export const Head = (props: HeadProps) => {
   const {
     ogImage = 'https://res.cloudinary.com/dpv0ukspz/image/upload/v1645574520/og_jsowpe.jpg',
-    title = 'Raymarching engine visualiser Sandbox',
+    title = 'Raymarching engine visualiser',
     description = 'Interactive visualiser 🌠 for a 3D raymarching engine written in GLSL and computed in a fragment shader on the 2D Plane.',
   } = props;
 
