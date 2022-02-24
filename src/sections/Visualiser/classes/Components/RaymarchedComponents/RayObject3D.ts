3c8b2fc953ce056eebb12e5af323099e4558e72c
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-24 15:12:29 +0100

    Fix color tint

--- src/sections/Visualiser/classes/Components/RaymarchedComponents/RayObject3D.ts
+++ src/sections/Visualiser/classes/Components/RaymarchedComponents/RayObject3D.ts
@@ -6,8 +6,6 @@ import { InteractiveObject3D } from '../InteractiveObject3D';
 
 export class RayObject3D extends InteractiveObject3D {
   _material = new THREE.MeshStandardMaterial({
-    transparent: true,
-    opacity: 0.8,
     color: '#ffffff',
   });
 
